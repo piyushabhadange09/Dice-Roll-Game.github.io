a80cc73562b2b5834fcd19b084d67f526a1c5e4c
PIYUSHA BHADANGE <116305709+12piyusha@users.noreply.github.com>
2024-06-04 09:37:28 +0530

    Add files via upload
    In this Dice Roll Game , two dices are present with respective to 2 players. The given dices is rolled by player . The player who has larger number on his dice between 1 to 6 will be the winner of that turn in game . If both have same number on dice, then the game will be tie up. We have to start from begin. Let's Roll a Dice to Play a Game!

---
+++ new_dice.js
@@ -0,0 +1,21 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+var radomdiceimage="dice"+randomnumber1+".png";
+var randomimagesource="images/"+radomdiceimage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomimagesource);
+
+var randomnumber2=Math.floor(Math.random()*6)+1;
+var randomimagesource2="images/dice"+randomnumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomimagesource2);
+
+if(randomnumber1 > randomnumber2)
+{
+    document.querySelector("h1").innerHTML="Player 1 Wins";
+}
+else if(randomnumber1 < randomnumber2)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins ";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
